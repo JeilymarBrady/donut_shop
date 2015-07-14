e4323d8be24ea22cabc33f82c6f7eb9a4a4f63c0
JEILYMAR BRADY <jeilymarb@gmail.com>
2015-07-14 14:02:50 -0700

    included bolding

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Donut Shop
 Working donut shop, yay!
+071315
+
+Cleaned it up, removed unneccessary code and if/else statements
+071415
--- shops.js
+++ shops.js
@@ -5,9 +5,6 @@ var Shop = function(shopName, minCustomers, maxCustomers, avgDonuts){
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgDonuts = avgDonuts;
-  this.opens = 700;
-  this.closes = 1800;
-  this.hoursOpen = (this.closes - this.opens)/100;
   this.hourly = [];
   this.totalDonuts = 0;
 };
@@ -18,18 +15,14 @@ Shop.prototype.render = function(){
 };
 
 Shop.prototype.donutsPerDay = function(){
-
   //Fills in the array 'hourly'
-  for(var i = 0; i < this.hoursOpen; i++){
+  for(var i = 0; i < 11; i++){
     this.hourly[i] = (Math.floor((Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers) * this.avgDonuts));
   }
-  console.log(this.hourly + this.shopName + ' hourly' );
-
   //The sum of the array 'hourly' to get the total number of donuts that day
-  for (var j=0; j<this.hoursOpen; j++){
+  for (var j = 0; j < this.hourly.length; j++){
     this.totalDonuts += this.hourly[j];
   }
-  console.log(this.totalDonuts + ' totalDonuts');
 };
 
 //Finds body in html and creates a table
@@ -37,34 +30,31 @@ var body = document.body;
 var tbl  = document.createElement('table');
 
 //Creates the header for the table: 'hours, times, total'
-var createTable = function(){
+var createTable = (function(){
   tbl.style.width  = '100px';
-  tbl.style.border = '1px dashed black';
+  tbl.style.border = '1px solid black';
   var tr = tbl.insertRow();
-  var rowData = "<td style: bold>" + 'Shops' + "</td>";
+  var rowData = "<td><b>" + 'Shops' + "</b></td>";
   for(var i = 0; i < 11; i++){
     var time = 700 + (i*100);
     rowData += "<td>" + time + "</td>";
   }
-  tr.innerHTML = rowData + "<td>" + 'Totals' + "</td>";
+  tr.innerHTML = rowData + "<td><b>" + 'Totals' + "</b></td>";
   tbl.appendChild(tr);
   body.appendChild(tbl);
-};
+})();
 
 //Creates a row for the given shop and fills in the corresponding data (aka magic)
 Shop.prototype.insertShop = function(){
   this.donutsPerDay();
   var tr = tbl.insertRow();
-  var rowData = "<td>" + this.shopName + "</td>";
+  var rowData = "<td><b>" + this.shopName + "</b></td>";
   for(var i = 0; i < this.hourly.length; i++){
     rowData += "<td>" + this.hourly[i] + "</td>";
   }
   tr.innerHTML = rowData + "<td>" + this.totalDonuts + "</td>";
   tbl.appendChild(tr);
 };
-
-//Creates the header and main table
-createTable();
 
 //Declaring and instantiating new shops --> calling render to add to table
 var downtown = new Shop('Downtown', 8, 43, 4.5);
